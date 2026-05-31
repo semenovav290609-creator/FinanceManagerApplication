959ade673a536edd3c670c4e9e36661ca158c990
semenovav290609-creator <semenovav290609@gmail.com>
2026-05-31 21:14:54 +0700

    Добавьте файлы проекта.

---
+++ FinanceManager/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace FinanceManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.totalSpent = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.numericUpDownPrice = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxCatigory = new System.Windows.Forms.ComboBox();
+            this.btnAddSpending = new System.Windows.Forms.Button();
+            this.lblBalance = new System.Windows.Forms.Label();
+            this.listViewTransactions = new System.Windows.Forms.ListView();
+            this.lblTransactionsTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(28)))), ((int)(((byte)(185)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 47);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " МЕНЕДЖЕР ФИНАНСОВ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalSpent
+            // 
+            this.totalSpent.AutoSize = true;
+            this.totalSpent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.totalSpent.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold);
+            this.totalSpent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(43)))));
+            this.totalSpent.Location = new System.Drawing.Point(530, 47);
+            this.totalSpent.MinimumSize = new System.Drawing.Size(267, 39);
+            this.totalSpent.Name = "totalSpent";
+            this.totalSpent.Size = new System.Drawing.Size(267, 39);
+            this.totalSpent.TabIndex = 2;
+            this.totalSpent.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.totalSpent.Click += new System.EventHandler(this.totalSpent_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(1, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 26);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Добавить новую трату";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(1, 121);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(267, 20);
+            this.textBoxName.TabIndex = 5;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            this.textBoxName.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // numericUpDownPrice
+            // 
+            this.numericUpDownPrice.Location = new System.Drawing.Point(1, 147);
+            this.numericUpDownPrice.Maximum = new decimal(new int[] {
+            -727379969,
+            232,
+            0,
+            0});
+            this.numericUpDownPrice.Name = "numericUpDownPrice";
+            this.numericUpDownPrice.Size = new System.Drawing.Size(267, 20);
+            this.numericUpDownPrice.TabIndex = 6;
+            this.numericUpDownPrice.ValueChanged += new System.EventHandler(this.numericUpDownPrice_ValueChanged);
+            // 
+            // comboBoxCatigory
+            // 
+            this.comboBoxCatigory.FormattingEnabled = true;
+            this.comboBoxCatigory.Location = new System.Drawing.Point(1, 173);
+            this.comboBoxCatigory.Name = "comboBoxCatigory";
+            this.comboBoxCatigory.Size = new System.Drawing.Size(267, 21);
+            this.comboBoxCatigory.TabIndex = 7;
+            this.comboBoxCatigory.SelectedIndexChanged += new System.EventHandler(this.comboBoxCatigory_SelectedIndexChanged);
+            // 
+            // btnAddSpending
+            // 
+            this.btnAddSpending.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnAddSpending.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddSpending.Location = new System.Drawing.Point(1, 200);
+            this.btnAddSpending.Name = "btnAddSpending";
+            this.btnAddSpending.Size = new System.Drawing.Size(267, 32);
+            this.btnAddSpending.TabIndex = 8;
+            this.btnAddSpending.Text = "Добавить трату";
+            this.btnAddSpending.UseVisualStyleBackColor = false;
+            this.btnAddSpending.Click += new System.EventHandler(this.btnAddSpending_Click);
+            // 
+            // lblBalance
+            // 
+            this.lblBalance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(242)))), ((int)(((byte)(255)))));
+            this.lblBalance.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBalance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.lblBalance.Location = new System.Drawing.Point(1, 46);
+            this.lblBalance.Name = "lblBalance";
+            this.lblBalance.Size = new System.Drawing.Size(267, 39);
+            this.lblBalance.TabIndex = 9;
+            this.lblBalance.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblBalance.Click += new System.EventHandler(this.lblBalance_Click);
+            // 
+            // listViewTransactions
+            // 
+            this.listViewTransactions.BackColor = System.Drawing.Color.White;
+            this.listViewTransactions.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listViewTransactions.Font = new System.Drawing.Font("Segoe UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listViewTransactions.FullRowSelect = true;
+            this.listViewTransactions.GridLines = true;
+            this.listViewTransactions.HideSelection = false;
+            this.listViewTransactions.Location = new System.Drawing.Point(274, 89);
+            this.listViewTransactions.Name = "listViewTransactions";
+            this.listViewTransactions.Size = new System.Drawing.Size(250, 143);
+            this.listViewTransactions.TabIndex = 10;
+            this.listViewTransactions.UseCompatibleStateImageBehavior = false;
+            this.listViewTransactions.View = System.Windows.Forms.View.Tile;
+            this.listViewTransactions.SelectedIndexChanged += new System.EventHandler(this.listViewTransactions_SelectedIndexChanged);
+            // 
+            // lblTransactionsTitle
+            // 
+            this.lblTransactionsTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTransactionsTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTransactionsTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblTransactionsTitle.Location = new System.Drawing.Point(274, 46);
+            this.lblTransactionsTitle.Name = "lblTransactionsTitle";
+            this.lblTransactionsTitle.Size = new System.Drawing.Size(250, 39);
+            this.lblTransactionsTitle.TabIndex = 11;
+            this.lblTransactionsTitle.Text = "История транзакций";
+            this.lblTransactionsTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTransactionsTitle);
+            this.Controls.Add(this.listViewTransactions);
+            this.Controls.Add(this.lblBalance);
+            this.Controls.Add(this.btnAddSpending);
+            this.Controls.Add(this.comboBoxCatigory);
+            this.Controls.Add(this.numericUpDownPrice);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.totalSpent);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label totalSpent;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrice;
+        private System.Windows.Forms.ComboBox comboBoxCatigory;
+        private System.Windows.Forms.Button btnAddSpending;
+        private System.Windows.Forms.Label lblBalance;
+        private System.Windows.Forms.ListView listViewTransactions;
+        private System.Windows.Forms.Label lblTransactionsTitle;
+    }
+}
+
---
+++ FinanceManager/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Web.Script.Serialization; // Заменили встроенную библиотеку JSON на стандартную для .NET Framework
+
+namespace FinanceManager
+{
+    public partial class Form1 : Form
+    {
+        // Подключаем системную функцию Windows для красивой подсказки в TextBox
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
+        private string filePath = "finance_data.json";
+
+        // Переменные для хранения данных
+        private int balance = 5000;
+        private int totalSp = 0;
+        private List<string> categories = new List<string>()
+        {
+            "Продукты", "Кафе и рестораны", "Транспорт", "Жилье и ЖКХ",
+            "Здоровье и аптека", "Одежда и обувь", "Развлечения",
+            "Техника", "Подарки", "Прочее"
+        };
+
+        // Специальный класс-модель для удобной упаковки данных в JSON
+        public class FinanceSaveModel
+        {
+            public int CurrentBalance { get; set; }
+            public int TotalSpent { get; set; }
+            public List<TransactionModel> Transactions { get; set; } = new List<TransactionModel>();
+        }
+
+        // Модель для отдельной транзакции
+        public class TransactionModel
+        {
+            public string Name { get; set; }
+            public string Price { get; set; }
+            public string Category { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDataFromJson(); // Загружаем данные из JSON
+
+            lblBalance.Text = $"Общий баланс: {balance}₽";
+            totalSpent.Text = $"Итого потрачено: {totalSp}.00 ₽";
+
+            // Настройка колонок для режима Tile
+            listViewTransactions.Columns.Add("Название", 150);
+            listViewTransactions.Columns.Add("Цена", 100);
+            listViewTransactions.Columns.Add("Категория", 150);
+
+            // Включаем водяной знак для поля названия
+            SendMessage(textBoxName.Handle, 0x1501, 1, "Название");
+
+            comboBoxCatigory.Items.Clear();
+            comboBoxCatigory.Items.AddRange(categories.ToArray());
+
+            comboBoxCatigory.SelectedIndex = -1;
+            comboBoxCatigory.Text = "Категория";
+        }
+
+        private void btnAddSpending_Click(object sender, EventArgs e)
+        {
+            int currentSpending = (int)numericUpDownPrice.Value;
+
+            if (currentSpending <= 0)
+            {
+                MessageBox.Show("Введите сумму траты больше нуля!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (comboBoxCatigory.SelectedIndex == -1 || comboBoxCatigory.Text == "Категория")
+            {
+                MessageBox.Show("Пожалуйста, выберите категорию траты!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            balance = balance - currentSpending;
+            totalSp = totalSp + currentSpending;
+
+            lblBalance.Text = $"Общий баланс: {balance}₽";
+            totalSpent.Text = $"Итого потрачено: {totalSp}.00 ₽";
+
+            string spendingName = string.IsNullOrWhiteSpace(textBoxName.Text) ? "Без названия" : textBoxName.Text;
+
+            ListViewItem item = new ListViewItem(spendingName);
+            item.SubItems.Add($"-{currentSpending} ₽");
+
+            item.UseItemStyleForSubItems = false;
+            item.SubItems[1].ForeColor = System.Drawing.Color.FromArgb(192, 57, 43);
+            item.SubItems[1].Font = new System.Drawing.Font("Segoe UI", 9, System.Drawing.FontStyle.Bold);
+
+            item.SubItems.Add(comboBoxCatigory.Text);
+            listViewTransactions.Items.Add(item);
+
+            numericUpDownPrice.Value = 0;
+            textBoxName.Text = "";
+            comboBoxCatigory.SelectedIndex = -1;
+            comboBoxCatigory.Text = "Категория";
+        }
+
+        // Автоматическое сохранение в JSON ПРИ ЗАКРЫТИИ программы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                // 1. Создаем объект нашей модели и заполняем текущими данными
+                FinanceSaveModel saveData = new FinanceSaveModel
+                {
+                    CurrentBalance = balance,
+                    TotalSpent = totalSp
+                };
+
+                // 2. Собираем все транзакции из ListView в список объектов
+                foreach (ListViewItem item in listViewTransactions.Items)
+                {
+                    saveData.Transactions.Add(new TransactionModel
+                    {
+                        Name = item.Text,
+                        Price = item.SubItems[1].Text,
+                        Category = item.SubItems[2].Text
+                    });
+                }
+
+                // 3. Используем JavaScriptSerializer для превращения объекта в строку
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                string jsonString = serializer.Serialize(saveData);
+
+                // 4. Записываем строку в файл
+                File.WriteAllText(filePath, jsonString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении JSON: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Метод для чтения данных из JSON ПРИ ЗАПУСКЕ программы
+        private void LoadDataFromJson()
+        {
+            if (!File.Exists(filePath)) return; // Если файла нет, выходим
+
+            try
+            {
+                // 1. Читаем весь текст из файла
+                string jsonString = File.ReadAllText(filePath);
+
+                // 2. Десериализуем (восстанавливаем данные)
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                FinanceSaveModel loadedData = serializer.Deserialize<FinanceSaveModel>(jsonString);
+
+                if (loadedData != null)
+                {
+                    // 3. Распределяем данные по переменным
+                    balance = loadedData.CurrentBalance;
+                    totalSp = loadedData.TotalSpent;
+
+                    // 4. Восстанавливаем транзакции на плитках
+                    foreach (var tx in loadedData.Transactions)
+                    {
+                        ListViewItem item = new ListViewItem(tx.Name);
+                        item.SubItems.Add(tx.Price);
+
+                        item.UseItemStyleForSubItems = false;
+                        item.SubItems[1].ForeColor = System.Drawing.Color.FromArgb(192, 57, 43);
+                        item.SubItems[1].Font = new System.Drawing.Font("Segoe UI", 9, System.Drawing.FontStyle.Bold);
+
+                        item.SubItems.Add(tx.Category);
+                        listViewTransactions.Items.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке JSON: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Пустые методы для дизайнера (не удалять)
+        private void lblBalance_Click(object sender, EventArgs e) { }
+        private void numericUpDownPrice_ValueChanged(object sender, EventArgs e) { }
+        private void totalSpent_Click(object sender, EventArgs e) { }
+        private void comboBoxCatigory_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void listViewTransactions_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void textBox1_Enter(object sender, EventArgs e) { }
+        private void textBoxName_TextChanged(object sender, EventArgs e) { }
+    }
+}
