5ce4275da89e529a344257c6becf70da09f8c06a
semenovav290609-creator <semenovav290609@gmail.com>
2026-06-01 01:09:16 +0700

    Сменил иконку приложения

--- FinanceManager/Form1.Designer.cs
+++ FinanceManager/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.totalSpent = new System.Windows.Forms.Label();
             this.textBoxName = new System.Windows.Forms.TextBox();
@@ -229,6 +230,7 @@
             this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.totalSpent);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).EndInit();
